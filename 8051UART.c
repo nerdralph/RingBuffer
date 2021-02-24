0c42be0240ecdec84f72a22c687d5000a0f6c18a
Nerd Ralph <ralphdoncaster@gmail.com>
2021-02-24 19:03:08 -0400

    update to wraparound mode

--- 8051UART.c
+++ 8051UART.c
@@ -1,5 +1,5 @@
 // 2020 Ralph Doncaster
-// 8051 UART ring buffer for sdcc
+// 8051 UART ring buffer for sdcc with ring buffer in iram
 // 202008 untested beta
 #include <8051.h>
 #include "ringbuffer.h"
@@ -8,19 +8,16 @@
 void Uart0_ISR(void) __interrupt (SI0_VECTOR) __naked
 {
     __asm
-    jb ti, 04$                         // ignore transmit interrupt 
+    jb ti, 04$                          // ignore transmit interrupt 
     push psw
     push a
-	mov	a, _gRing                       // head
-	cjne a, (_gRing + 1),02$            // == tail?
-    sjmp 03$                            // buffer full
-02$:
+    mov	a, _ringHead
     add	a, #(_gRingBuf)
    	xch	a, r0
     mov	@r0, sbuf                       // save received byte
     xch	a, r0
-    inc _gRingBuf                       // increment head
-    anl	_gRingBuf, #(RINGBUFSIZE * 2 - 1)
+    inc _ringHead
+    anl	_gRingBuf, #(RINGBUFSIZE - 1)
 03$:
     clr ri                              // clear interrupt flag
     pop a
